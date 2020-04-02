050b0b8021fc09a637308743b5f02f3f747d9b60
T <gauquelintimothe@gmail.com>
2020-04-02 22:59:42 +0200

    New Pokemon Mock
    Used before the real one

---
+++ src/app/mock-pokemons.ts
@@ -0,0 +1,13 @@
+import { Pokemon } from './pokemon';
+
+export const POKEMONS: Pokemon[] = [
+  { id: 1, name: 'Bulbizarre', img:"/assets/1.png" },
+  { id: 2, name: 'Herbizarre', img:"assets/2.png" },
+  { id: 3, name: 'Florizarre', img:"assets/3.png" },
+  { id: 4, name: 'Salamèche', img:"assets/4.png" },
+  { id: 5, name: 'Reptincel', img:"assets/5.png" },
+  { id: 6, name: 'Dracaufeu', img:"assets/6.png" },
+  { id: 7, name: 'Carapuce', img:"assets/7.png" },
+  { id: 8, name: 'Carabaffe', img:"assets/8.png" },
+  { id: 9, name: 'Tortank', img:"assets/9.png" },
+]
