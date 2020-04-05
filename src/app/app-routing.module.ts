d6fb39aa79d0ee57b7fab93cb8b270d263c6ce6c
T <gauquelintimothe@gmail.com>
2020-04-05 17:50:38 +0200

    Add routes & nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IndexComponent } from './index/index.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PokemonDetailComponent }  from './pokemon-detail/pokemon-detail.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'index', component: IndexComponent},
+  { path: 'accueil', component: DashboardComponent },
+  { path: 'pokemon/:id', component: PokemonDetailComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
